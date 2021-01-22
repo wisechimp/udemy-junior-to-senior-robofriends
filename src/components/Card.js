76d9e3e54c298c128d1e4a83b2da22008bd31bde
shicks1080 <stuart.hicks@gmail.com>
2021-01-22 15:13:12 +0100

    Create a card and pop it in the home page
    We create a card and populate it using an array of robot data which he provides. This gets popped straight in the index (gasp!).

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// We can destructure right at the top which saves us passing props then destructuring
+const Card = ({ id, name, email }) => {
+
+  return (
+    // Everything goes in the div as we can only return one element
+    <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img src={`https://robohash.org/${id}?200x200'`} alt={`${name}`}/>
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
