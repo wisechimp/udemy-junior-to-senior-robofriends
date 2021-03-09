36887e8d8fae67685bed85348a40722bcfbe4e0d
shicks1080 <stuart.hicks@gmail.com>
2021-03-09 11:47:47 +0100

    Adding style to the app.
    Background and a custom font for the title. Changing the card background to a more suitable colour to fit the background.

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ id, name, email }) => {
 
   return (
     // Everything goes in the div as we can only return one element
-    <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img src={`https://robohash.org/${id}?200x200'`} alt={`${name}`}/>
       <div>
         <h2>{name}</h2>
