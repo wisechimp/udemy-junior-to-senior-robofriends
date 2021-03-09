858272c3280364d71d179754a1c10f6cad7e2949
shicks1080 <stuart.hicks@gmail.com>
2021-03-09 12:33:09 +0100

    We add a component to scroll through the robots and retain the title and search box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import CardList from './components/CardList'
+import Scroll from './components/Scroll'
 import SearchBox from './components/SearchBox'
 
 import './App.css'
@@ -33,7 +34,9 @@ class App extends Component {
     <div className='tc'>
       <h1 className='f1'>RoboFriends</h1>
       <SearchBox searchChange={this.onSearchChange} />
-      <CardList robots={filteredRobots} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
     </div>
     )
   }
