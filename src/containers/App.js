d6695fa7476f27d08dcc983801a5a0b8c6bd68bd
shicks1080 <stuart.hicks@gmail.com>
2021-03-23 11:32:57 +0100

    Adding redux thunk and using it to fetch the robots.
    Adding the thunk middleware and a rootReducer, containing all the reducers in the project, to the index.
    Adding new constants for the different phases and possibilities when the robots are fetched.
    Adding an action for fetching the robots with a catch.
    Adding the reducer for managing the results of fetching the robots.
    Wiring up the App by removing the original state.robots and replacing it with the action and the state.

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,42 +6,38 @@ import ErrorBoundary from '../components/ErrorBoundary'
 import Scroll from '../components/Scroll'
 import SearchBox from '../components/SearchBox'
 
-import { setSearchField } from '../actions'
+import { requestRobots, setSearchField } from '../actions'
 
 import './App.css'
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
+    searchField: state.searchRobots.searchField
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {onSearchChange: (event) => dispatch(setSearchField(event.target.value))}
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
 }
 
-class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      robots: []
-    }
-  }
-
+class App extends Component { 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }))
+    this.props.onRequestRobots()
   }
 
   render() {
-    const { robots } = this.state
-    const { searchField, onSearchChange } = this.props
+    const { searchField, onSearchChange, robots, isPending } = this.props
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
 
-    return !robots.length ?
+    return isPending ?
       <h1>Loading...</h1> : 
       (
         <div className='tc'>
