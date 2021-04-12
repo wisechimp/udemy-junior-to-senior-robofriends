e3de35cfdc770777b2d275be9842db4f25a0d6d7
shicks1080 <stuart.hicks@gmail.com>
2021-04-12 15:16:30 +0200

    Adding a service worker to the project

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import 'tachyons'
 
 import App from './containers/App'
@@ -23,6 +24,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
