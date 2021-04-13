195badcd915f0a2395f56a2c514b9b2ad25081e6
shicks1080 <stuart.hicks@gmail.com>
2021-04-13 13:17:48 +0200

    Addressing Lighthouse report issues
    Mainly adding icons!

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ id, name, email }) => {
   return (
     // Everything goes in the div as we can only return one element
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}?200x200'`} alt={`${name}`}/>
+      <img src={`https://robohash.org/${id}?size=200x200`} alt={`${name}`} width="200"/>
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
