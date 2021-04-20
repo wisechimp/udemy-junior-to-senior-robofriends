395147a505ff9ee96045aa81cfaf236d849fe0a9
shicks1080 <stuart.hicks@gmail.com>
2021-04-20 11:03:25 +0200

    Moving Home Page content into a component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import CardList from '../components/CardList'
-import ErrorBoundary from '../components/ErrorBoundary'
-import Header from '../components/Header'
-import Scroll from '../components/Scroll'
-import SearchBox from '../components/SearchBox'
-
 import { requestRobots, setSearchField } from '../actions'
-
-import './App.css'
+import PageContent from '../components/PageContent'
 
 const mapStateToProps = state => {
   return {
@@ -28,29 +21,8 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component { 
-  componentDidMount() {
-    this.props.onRequestRobots()
-  }
-
   render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase())
-    })
-
-    return isPending ?
-      <h1>Loading...</h1> : 
-      (
-        <div className='tc'>
-          <Header />
-          <SearchBox searchChange={onSearchChange} />
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundary>
-          </Scroll>
-        </div>
-      )
+    return <PageContent { ...this.props }/>
   }
 }
 
