6cf7a75497ba4ac367252797fe15d6462640c03d
Stuart Hicks <stuart.hicks@gmail.com>
2022-02-14 14:59:06 +0100

    Prettier test

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
 // We can destructure right at the top which saves us passing props then destructuring
 const Card = ({ id, name, email }) => {
-
   return (
     // Everything goes in the div as we can only return one element
-    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}?size=200x200`} alt={`${name}`} width="200"/>
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img
+        src={`https://robohash.org/${id}?size=200x200`}
+        alt={`${name}`}
+        width="200"
+      />
       <div>
-        <h2>{name}</h2>
+        <h2>{name} </h2>
         <p>{email}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
